797803cc26c2b46bf5e1209137a8244ab3817a4a
Kostya Sergeev <kostyasergeev93@gmail.com>
2014-04-01 23:03:49 +0300

    project settings

---
+++ SPO-course-project/Logic/Interface.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Management;
+
+namespace SPO_course_project.Logic
+{
+    public interface IArOperation
+    {
+        ManagementObjectSearcher getProcessList();
+    }
+
+    class GetPrpcessInfo : IArOperation
+    {
+        public virtual ManagementObjectSearcher getProcessList()
+        {
+        ManagementObjectSearcher searcher = new ManagementObjectSearcher("root\\CIMV2","Select Name, CommandLine From Win32_Process");
+        return searcher;
+    }
+
+            /*foreach (ManagementObject instance in searcher.Get())
+            {
+                Console.WriteLine("{0}", instance["Name"]);
+            }
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);*/
+       
+    }
+}
