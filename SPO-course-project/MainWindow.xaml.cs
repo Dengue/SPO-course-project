4b10fc3500ced40a9eb8b7367bf0d45e1158f14b
Kostya Sergeev <kostyasergeev93@gmail.com>
2014-04-27 17:32:34 +0300

    temporary gui

--- SPO-course-project/MainWindow.xaml.cs
+++ SPO-course-project/MainWindow.xaml.cs
@@ -17,64 +17,7 @@ namespace SPO_course_project
         public MainWindow()
         {
             NavigationController Singleton = NavigationController.GetInstance();
-            this.NavigationService.Navigate(Singleton.page1);
-
-            /*ProcessData.EventHandler = new ProcessData.MyEvent(toDataGrid);
-            SoftData.EventHandler = new SoftData.MyEvent(toDataGrid);
-            DrivesData.EventHandler = new DrivesData.MyEvent(toDataGrid);
-            GraphicData.EventHandler = new GraphicData.MyEvent(toDataGrid);
-            MemoryData.EventHandler = new MemoryData.MyEvent(toDataGrid);
-            CPUData.EventHandler = new CPUData.MyEvent(toDataGrid);
-            OSData.EventHandler = new OSData.MyEvent(toDataGrid);
-
-            InitializeComponent();
-
-            Processes myList = new Processes();
-            IArOperation Interface = new ImplementationClass();
-            //Interface.getProcessList();
-            //Interface.getSoftInfo();
-            //Interface.getDrivesInfo();
-            //Interface.getGraphicsInfo();
-            //Interface.getMemoryInfo();
-            //Interface.getProcessorInfo();
-            Interface.getOSInfo();*/
-
+            this.NavigationService.Navigate(Singleton.page1);          
         }
-
-
-        /*public void toDataGrid(Processes myList)
-        {            
-            dataGrid1.ItemsSource = myList.processList;
-        }
-
-        public void toDataGrid(Soft myList)
-        {
-            dataGrid1.ItemsSource = myList.softList;
-        }
-
-        public void toDataGrid(Drives myList)
-        {
-            dataGrid1.ItemsSource = myList.drivesList;
-        }
-
-        public void toDataGrid(GraphicAdapters myList)
-        {
-            dataGrid1.ItemsSource = myList.adptersList;
-        }
-
-        public void toDataGrid(MemoryBanks myList)
-        {
-            dataGrid1.ItemsSource = myList.banksList;
-        }
-
-        public void toDataGrid(CPUList myList)
-        {
-            dataGrid1.ItemsSource = myList.cpuList;
-        }
-
-        public void toDataGrid(OSList myList)
-        {
-            dataGrid1.ItemsSource = myList.osList;
-        }*/
     }
 }
--- SPO-course-project/View/MainPage.xaml.cs
+++ SPO-course-project/View/MainPage.xaml.cs
@@ -13,6 +13,11 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using SPO_course_project.Logic;
+using SPO_course_project.Model;
+using SPO_course_project.Controller;
+using SPO_course_project.View;
+
 namespace SPO_course_project.View
 {
     /// <summary>
@@ -20,9 +25,92 @@ namespace SPO_course_project.View
     /// </summary>
     public partial class MainPage : Page
     {
+        IArOperation Interface = new ImplementationClass();
+
         public MainPage()
         {
+            ProcessData.EventHandler = new ProcessData.MyEvent(toDataGrid);
+            SoftData.EventHandler = new SoftData.MyEvent(toDataGrid);
+            DrivesData.EventHandler = new DrivesData.MyEvent(toDataGrid);
+            GraphicData.EventHandler = new GraphicData.MyEvent(toDataGrid);
+            MemoryData.EventHandler = new MemoryData.MyEvent(toDataGrid);
+            CPUData.EventHandler = new CPUData.MyEvent(toDataGrid);
+            OSData.EventHandler = new OSData.MyEvent(toDataGrid);
+
             InitializeComponent();
+        }
+
+
+        private void graphics_click(object sender, RoutedEventArgs e)
+        {
+            Interface.getGraphicsInfo();
+        }
+
+        private void cpuButton_Click(object sender, RoutedEventArgs e)
+        {
+            Interface.getProcessorInfo();
+        }
+
+        private void drivesButton_Click(object sender, RoutedEventArgs e)
+        {
+            Interface.getDrivesInfo();
+        }
+
+        private void memoryButton_Click(object sender, RoutedEventArgs e)
+        {
+            Interface.getMemoryInfo();
+        }
+
+        private void osButton_Click(object sender, RoutedEventArgs e)
+        {
+            Interface.getOSInfo();
+        }
+
+        private void processesButton_Click(object sender, RoutedEventArgs e)
+        {
+            Interface.getProcessList();
+        }
+
+        private void softButton_Click(object sender, RoutedEventArgs e)
+        {
+            Interface.getSoftInfo();
+        }
+
+        //-----------------------------------------------------//
+
+        public void toDataGrid(Processes myList)
+        {            
+            dataGrid1.ItemsSource = myList.processList;
+        }
+
+        public void toDataGrid(Soft myList)
+        {
+            dataGrid1.ItemsSource = myList.softList;
+        }
+
+        public void toDataGrid(Drives myList)
+        {
+            dataGrid1.ItemsSource = myList.drivesList;
+        }
+
+        public void toDataGrid(GraphicAdapters myList)
+        {
+            dataGrid1.ItemsSource = myList.adptersList;
+        }
+
+        public void toDataGrid(MemoryBanks myList)
+        {
+            dataGrid1.ItemsSource = myList.banksList;
+        }
+
+        public void toDataGrid(CPUList myList)
+        {
+            dataGrid1.ItemsSource = myList.cpuList;
+        }
+
+        public void toDataGrid(OSList myList)
+        {
+            dataGrid1.ItemsSource = myList.osList;
         }
     }
 }
